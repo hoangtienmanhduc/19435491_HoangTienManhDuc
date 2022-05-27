53e9417ff62c3a6d5618a92fd768a7400114ddf3
Admin <khongcotinhban@gmail.com>
2022-05-27 09:00:43 +0700

    commit4

--- app/src/main/java/com/example/a13_19435491_hoangtienmanhduc/MainActivity.java
+++ app/src/main/java/com/example/a13_19435491_hoangtienmanhduc/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.a13_19435491_hoangtienmanhduc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tong;
+    int tongsp = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tong = findViewById(R.id.tong);
+
+
     }
+
 }
